c17f4ed5410a2c1943655f8880ea1a3131ed82b6
Pankaj Pawar <Pankaj14321@protonmail.com>
2025-02-04 06:27:39 +0000

    client error solved

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import {HeroUIProvider, Button} from "@heroui/react";
+import {HeroUIProvider} from "@heroui/react";
 import Emoji from "../components/Emoji";
-import { useState } from "react";
+\
 
 
 export default function App() {
 
 
-  const handleClick = () => {
-    
-  };
+  
  
   return (
     <HeroUIProvider>
